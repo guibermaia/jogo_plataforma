2af04e681afe4d0c9eb1d73182e18d0997103144
guibermaia <guilhermebertoldi21384@gmail.com>
2020-12-01 15:29:30 -0300

    first commit

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+
+	public int coinValue = 1;
+	
+	private void OnTriggerEnder2D(Collider2D col){
+
+		if(col.gameObject.CompareTag("Coins")){
+
+			Destroy(col.gameObject);
+		}
+
+		if(col.gameObject.CompareTag("Player")){
+
+			ScoreManager.instance.ChangeScore(coinValue);
+		}
+	}
+}
+
+	// Use this for initialization
